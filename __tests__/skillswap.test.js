449c3a08ed04bf49716f35a900cf388042d567a6
Freddie Alvarez <alvarez.freddie@ic.edu>
2026-02-05 21:44:10 -0600

    Write Filter Skills Failing Tests

--- __tests__/skillswap.test.js
+++ __tests__/skillswap.test.js
@@ -1,3 +1,29 @@
 test("test environment is set up", () => {
   expect(true).toBe(true);
 });
+
+const skills = [
+  { title: 'Python Tutoring', category: 'Programming', price: 20 },
+  { title: 'Guitar Lessons', category: 'Music', price: 15 },
+  { title: 'Resume Review', category: 'Career', price: 0 },
+  { title: 'Web Development', category: 'Programming', price: 25 }
+];
+
+filterSkillsByCategory(skills, 'Programming'); 
+/* [
+  { title: 'Python Tutoring', category: 'Programming', price: 20 },
+  { title: 'Web Development', category: 'Programming', price: 25 }
+]
+*/
+
+filterSkillsByCategory(skills, 'All'); 
+/*
+[
+  { title: 'Python Tutoring', category: 'Programming', price: 20 },
+  { title: 'Guitar Lessons', category: 'Music', price: 15 },
+  { title: 'Resume Review', category: 'Career', price: 0 },
+  { title: 'Web Development', category: 'Programming', price: 25 }
+];
+*/
+
+filterSkillsByCategory(skills, 'Cooking'); // []
